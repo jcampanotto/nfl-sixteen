14d6058ff9369891f13466a7d7e2a44a78ba8811
Joseph Campanotto <joe-laptop@Josephs-MBP.attlocal.net>
2022-07-27 20:39:00 -0500

    Going to redesign around breakpoint.

--- src/components/WeekDropdown.jsx
+++ src/components/WeekDropdown.jsx
@@ -1,7 +1,11 @@
 function WeekDropdown() {
   return (
     <div className="weekDropdownContainer dropdown">
-      <label tabIndex={0} className="btn m-1">Click</label>
+      <label tabIndex="0" className="btn btn-primary m-1">Click</label>
+      <ul tabIndex="0" className="dropdown-content menu p-2 shadow bg-primary rounded-box">
+        <button className="btn btn-primary btn-xs">W1</button>
+        
+      </ul>
 
     </div>
   )
