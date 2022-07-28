2d7884dd6bc0e2953b74d8ad8c2abab2cd4dfcb4
Joseph Campanotto <joe-laptop@Josephs-MBP.attlocal.net>
2022-07-27 20:21:52 -0500

    Added custom breakpoint and WeekDropdown component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
+import WeekBubbles from "./components/WeekBubbles";
+import WeekDropdown from "./components/WeekDropdown";
 
 function App() {
   return (
     <>
     <div className='flex flex-col justify-between h-screen'>
       <Navbar/>
-      <main className='container mx-auto px-3 pb-12'> content</main>
+      <main className='container mx-auto px-3 pb-12 h-full'>
+        <WeekDropdown/>
+        <WeekBubbles/>
+      </main>
       <Footer/>
     </div>
     </>
---
+++ src/components/WeekDropdown.jsx
@@ -0,0 +1,10 @@
+function WeekDropdown() {
+  return (
+    <div className="weekDropdownContainer dropdown">
+      <label tabIndex={0} className="btn m-1">Click</label>
+
+    </div>
+  )
+}
+
+export default WeekDropdown
