0a1e61b29072f0e0c35ff1b0dbe651bdabf86fab
Joseph Campanotto <joe-laptop@Josephs-MBP.attlocal.net>
2022-07-27 19:23:57 -0500

    Added date to footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,10 +1,11 @@
 import logo from '../../images/logo.png'
 
 function Footer() {
+  const footerYear = new Date().getFullYear()
   return (
-    <footer className="footer p-2 pt-10 bg-base-300 text-primary-content footer-center">
-      <img alt="NFL Logo" src={logo} className="mx-auto h-12 w-12 fill-current"></img>
-      <p className='pb-2'> All logos are trademark of the NFL. <br/>This website doesn't earn a dime. <br/> Date</p>
+    <footer className="footer p-2 pt-6 bg-base-300 text-primary-content footer-center block">
+      <img alt="NFL Logo" src={logo} className="mx-auto h-12 w-12 fill-current mb-5"></img>
+      <p className='pb-2'> All logos are trademark of the NFL <br/>This website doesn't earn a dime <br/> {footerYear} </p>
     </footer>
   )
 }
